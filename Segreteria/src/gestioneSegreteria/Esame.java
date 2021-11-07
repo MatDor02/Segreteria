e7d8127f08d45010b5888beab9a2199aeefafd35
MatDor02 <93282445+MatDor02@users.noreply.github.com>
2021-11-07 12:22:16 +0100

    votationis in esame

--- Segreteria/src/gestioneSegreteria/Esame.java
+++ Segreteria/src/gestioneSegreteria/Esame.java
@@ -27,7 +27,7 @@ public class Esame extends Materia {
 	}
 	
 	public String  toString() {
-		return "Nome: " + nome + " ---|--- " + "Numero cfu: " + cfu + " ---|--- " + "Votazione: " + voto;
+		return "Nome: " + nome + " ---|--- " + "Numero cfu: " + cfu + " ---|--- " + "Votationis: " + voto;
 	}
 	
 }
