4cc2ab1053da4653e72bcc3674481bb8b2080d14
MatDor02 <93282445+MatDor02@users.noreply.github.com>
2021-11-07 11:57:59 +0100

    modifica toString di Esame
    cfu invece di crefditi nella stama a schermo

--- Segreteria/src/gestioneSegreteria/Esame.java
+++ Segreteria/src/gestioneSegreteria/Esame.java
@@ -27,7 +27,7 @@ public class Esame extends Materia {
 	}
 	
 	public String  toString() {
-		return "Nome: " + nome + " ---|--- " + "Numero crediti: " + cfu + " ---|--- " + "Votazione: " + voto;
+		return "Nome: " + nome + " ---|--- " + "Numero cfu: " + cfu + " ---|--- " + "Votazione: " + voto;
 	}
 	
 }
