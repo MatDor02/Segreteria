af5f7cdfad43c3ec7aff615d7ed2662e2bb4881d
Matteo D'Orazio <matteo.dorazio02@gmail.com>
2021-11-11 20:05:38 +0100

    Moved lauree into gestioneSegreteria package

--- Triennale.java
+++
@@ -1,9 +0,0 @@
-package segreteria;
-
-public class Triennale extends CorsoLaurea {
-	private String tipo = "Corso Triennale";
-	public Triennale (String nomeCorso, byte min_cfu, byte n_def, byte n_scl) {
-		super (nomeCorso, min_cfu, n_def, n_scl);
-	}
-
-}
