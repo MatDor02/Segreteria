ef68e4c657db9268e1a0e79ed3853ec7ae015b96
Matteo D'Orazio <matteo.dorazio02@gmail.com>
2021-11-17 19:56:41 +0100

    created DataFormatException

---
+++ Segreteria/src/myExceptions/DataFormatException.java
@@ -0,0 +1,11 @@
+package myExceptions;
+
+public class DataFormatException extends Exception{
+    public DataFormatException() {
+        super();
+    }
+
+    public DataFormatException(String message) {
+        super(message);
+    }
+}
