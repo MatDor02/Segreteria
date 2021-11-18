483c7c8df6761a59891cd496dd5b614f8a3f2e69
Matteo D'Orazio <matteo.dorazio02@gmail.com>
2021-11-18 08:20:15 +0100

    Created Event interface and have it implemented in Esame. Deleted GiornoEsame class

--- Segreteria/src/Insegnamenti/Esame.java
+++ Segreteria/src/Insegnamenti/Esame.java
@@ -1,17 +1,37 @@
 package Insegnamenti;
 
-public class Esame extends Materia {
+import Persone.Professore;
+import myInterfaces.Event;
+
+public class Esame extends Materia implements Event {
 
 	private byte cfu;
 	private byte voto;
 	private boolean superato;
-	private GiornoEsame giorno;
+	private String data, orario;
 	private short durata; // in minuti
 	
-	public Esame(String nomeEsame, byte crediti, short tempo, GiornoEsame giorno) {
-		super(nomeEsame, crediti);
+	public Esame(String nomeEsame, byte crediti, Professore prof, String data, String orario, short tempo) {
+		super(nomeEsame, crediti, prof);
 		durata = tempo;
-		this.giorno = giorno;
+		this.data = data;
+		this.orario = orario;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getOrario() {
+		return orario;
+	}
+
+	public void setOrario(String orario) {
+		this.orario = orario;
 	}
 	
 	public byte getVoto() {
@@ -31,7 +51,7 @@ public class Esame extends Materia {
 	}
 	
 	public String  toString() {
-		return "Data: " + giorno.getData() + "---|---" + "Nome: " + nome + " ---|--- " +
-				"Numero crediti: " + cfu + " ---|--- " + "Voto: " + voto;
+		return "Data: " + data + " alle " + orario + "---|---" + "Nome: " + nome + " ---|--- " +
+				"Voto: " + voto + " ---|--- " + "CFU: " + cfu;
 	}
 }
---
+++ Segreteria/src/myInterfaces/Event.java
@@ -0,0 +1,12 @@
+package myInterfaces;
+
+public interface Event {
+
+    public String getData();
+
+    public void setData(String data);
+
+    public String getOrario();
+
+    public void setOrario(String orario);
+}
