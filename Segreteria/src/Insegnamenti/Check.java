d5b2f061de85e15de7d0f70d46ca105b57c8c30c
Matteo D'Orazio <matteo.dorazio02@gmail.com>
2021-11-18 07:44:13 +0100

    implemented data method in Check class

--- Segreteria/src/Insegnamenti/Check.java
+++ Segreteria/src/Insegnamenti/Check.java
@@ -1,5 +1,7 @@
 package Insegnamenti;
 
+import myExceptions.DataFormatException;
+
 public class Check {
 	
 	public static boolean orario(String orario) { // controlla che la stringa sia un orario
@@ -28,9 +30,28 @@ public class Check {
 		}
 		return false;
 	}
+	// gg/mm/aa
+	public static void data(String data) throws DataFormatException {
+		boolean isOk = false;
+		if (data.length() == 8)
+			if(data.charAt(2) == '/' && data.charAt(5) == '/')
+				try {
+					if(Integer.parseInt(data.substring(6, 8)) == 21 || Integer.parseInt(data.substring(6, 8)) == 22) { // controlla l'anno
+						byte m = Byte.parseByte(data.substring(3, 5));
+						byte g = Byte.parseByte(data.substring(0, 2));
+						if(g > 0) {
+							switch (m) { // controllo il mese con i case dello switch e i giorni all'interno dei case
+								case 4, 6, 9, 11:  if (g <= 30) isOk = true;
+								case 2: if(g <= 28) isOk = true;
+								case 1, 3, 5, 7 ,8, 10, 12: if(g <= 31) isOk = true;
+							}
+						}
+					}
+				} catch (NumberFormatException e) {
 
-	public boolean isData(String data) {
-
+				}
+		if(!isOk)
+			throw new DataFormatException("Non e' una data esistente o selezionabile");
 	}
 	
 	public static boolean isEmpty(Object[] obj) {
