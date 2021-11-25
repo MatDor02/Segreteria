2936c623cd62ad088a704a10e01218f94e0c4917
Dan13L01 <daniel.tridici@hotmail.com>
2021-11-25 15:44:21 +0100

    inserimento del menù dei corsi nella classe GestioneMenù

--- src/gestioneMenu.java
+++ src/gestioneMenu.java
@@ -12,8 +12,114 @@ public class gestioneMenu {
     private static Professore profLoggato;
 
     public static void menuCorsoLaurea(CorsoLaurea corso) {
-        corso.menu_corso();
-    }
+              // MENU'
+		// TODO: per prima cosa leggiamo da file (CON SERIALIZZAZIONE) tutti gli attributi di questa classe.
+		// 		Al termine della funzione scriviamo gli attributi sui relativi file
+			System.out.println("CORSO DI " + corso.get_nomeCorso() + "[anno accademico 2021/2022]\n" + corso.getTipo());
+			boolean f = true;
+			do {
+				// TODO 1) sei uno studente? --> menu studente (gestione esami passati e futuri, stampa media, ecc.)
+				//		2) sei un professore? --> menu professore (seleziona una materia che insegna, aggiunge nuovi appelli)
+				//		3) sei un segretario? --> menu_corso() (gestisci studenti, modifica insegnamenti, orari, ...)
+				//		4) esercitatore? --> menu_sfigato()
+				//		5) goldrake? --> affila il ferro
+				//		6) e tanto altro ancora (consultare la documentazione)
+				//		......................
+				System.out.println("COMANDI GESTIONE STUDENTI: ");
+				System.out.println("I) Iscrivi uno studente a questo corso");
+				System.out.println("C) Cerca uno studente tra quelli iscritti");
+				System.out.println("R) Ritira uno studente");
+				System.out.println("V) Visualizza gli studenti iscritti al corso");
+				System.out.println("L) Laurea uno studente");
+				System.out.println("U) Visualizza gli studenti laureati al corso");
+				System.out.println("O) Cerca uno studente tra quelli laureati");
+				System.out.println("COMANDI GESTIONE MATERIE:");
+				System.out.println("Y) Inserisci un insegnamento di base");
+				System.out.println("G) Rimuovi un insegnamento di base");
+				System.out.println("J) Inserisci un insegnamento a scelta");
+				System.out.println("X) Rimuovi un insegnamento a scelta");
+				System.out.println("W) Modifica orario settimanale di una materia");
+				System.out.println("M) Visualizza le materie del corso");
+				System.out.println("\nE) Torna al menù precedente");
+
+				String ris = input.nextLine();
+				switch (ris) {
+					case "i", "I": {
+						System.out.println("Inserisci il nome e l'anno a cui iscrivere lo studente:\n-> ");
+						String nome = input.next();
+						corso.Iscrizione(nome);
+					}
+					break;
+					case "c", "C": {
+						System.out.println("Inserisci la matricola dello studente\n-> ");
+						String mat = input.next();
+						corso.CercaStudente(mat, studenti);
+					}
+					break;
+					case "r", "R": {
+						System.out.println("Inserisci la matricola dello studente\n-> ");
+						String matr = input.next();
+						corso.Ritiro(matr);
+					}
+					break;
+					case "v", "V":
+						corso.VisualizzaStudenti();
+						break;
+					case "l", "L": {
+						System.out.println("Inserisci la matricola dello studente\n-> ");
+						String matri = input.next();
+						System.out.println("Inserisci il voto finale da dare allo studente\n-> ");
+						byte voto = input.nextByte();
+						corso.LaureaStudente(CercaStudente(matri, studenti), voto);
+					}
+					break;
+					case "u", "U":
+						corso.VisualizzaLaureati();
+						break;
+					case "o", "O": {
+						System.out.println("Inserisci la matricola dello studente laureato\n-> ");
+						String matric = input.next();
+						corso.CercaStudente(matric, laureati);
+					}
+					break;
+					case "y", "Y":
+						corso.In_ins_def();
+						break;
+					case "g", "G":
+						corso.Out_ins_def();
+						break;
+					case "j", "J":
+						corso.In_ins_scl();
+						break;
+					case "x", "X":
+						corso.Out_ins_scl();
+						break;
+					case "w", "W": {
+						byte cont = 1;
+						for (byte x = 0; x < corso.get_ins_def().length; x++)
+							System.out.println(cont++ + ") " + corso.get_ins_def()[x].getNome());
+						for (byte x = 0; x < corso.get_ins_scl().length; x++)
+							System.out.println(cont++ + ") " + corso.get_ins_scl()[x].getNome());
+						System.out.println("Inserisci il numero corrispondente alla materia a cui si vuole accedere\n-> ");
+						byte res = input.nextByte();
+						if (res > (corso.get_ins_def().length + corso.get_ins_scl().length))
+							System.out.println("Utilizza solo numeri <= di " + cont);
+						else if (res < corso.get_ins_def().length) menuOrario(corso.get_ins_def()[res - 1]);
+						else menuOrario(corso.get_ins_scl()[res - corso.get_ins_def().length - 1]);
+					}
+					case "m", "M":
+						corso.VisualizzaMaterie();
+						break;
+					case "e", "E":
+						f = false;
+						break;
+					default:
+						System.out.println("Utilizza solo i caratteri consentiti!\n");
+				}
+			} while (f);
+			System.out.println("[---]");
+		}
+  
 
     public static void menuStudente(CorsoLaurea corso) {
         Scanner input = new Scanner(System.in);
